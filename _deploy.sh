f0bc757ee5fb101e5a92e6e633532ed76d5cb623
Hua <zouhua@xbiome.com>
2022-08-09 09:21:55 +0800

    Update the code

--- _deploy.sh
+++ _deploy.sh
@@ -1,5 +1,18 @@
+#!/bin/sh
 
-git add -A
-git commit -m "Update the book"
+set -e
+
+#[ -z "${GITHUB_PAT}" ] && exit 0
+#[ "${TRAVIS_BRANCH}" != "main" ] && exit 0
+
+#git config --global user.email "zouhua1@outlook.com"
+#git config --global user.name "Hua Zou"
+
+#git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+#cd book-output
+#cp -r ../_book/* ./
+git add --all *
+git commit -m "Update the code" || true
+#git push -q origin gh-pages
 git push origin main
 
